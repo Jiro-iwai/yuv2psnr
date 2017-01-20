5eac896c66c5709e90a20417b16a6ca494fa4eca
Jiro-iwai <jiro.iwai@gmail.com>
2017-01-21 02:12:33 +0900

    フィファイル名変更

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 all:
-	g++ -o yuv2psnr -O3 -std=c++11 yuv2psnr_single.cpp
+	g++ -o yuv2psnr -O3 -std=c++11 yuv2psnr.cpp
+
+.PHONY: test
 
 test:
 	cd test; make
--- yuv2psnr.cpp
+++ yuv2psnr.cpp
@@ -1,302 +1,190 @@
-// 
 
 #include <cfloat>
 #include <cmath>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <deque>
-#include <memory>
-#include <thread>
-#include <future>
-#include <sstream>
 #include <boost/lexical_cast.hpp>
 #include <boost/format.hpp>
 
 using namespace std;
 
-double MSE
-( const vector<unsigned char>& c0_vec
-, const vector<unsigned char>& c1_vec
+void MSE
+( unsigned char* c0
+, unsigned char* c1
+, unsigned int min_read_unit
+, double& MSE
 )
 {
-    double MSE = 0.0;
-    auto i0 = c0_vec.begin();
-    auto i1 = c1_vec.begin();
-    auto end = c0_vec.end();
-    while( i0 != end ) {
-        unsigned char I = *i0++;
-        unsigned char K = *i1++;
-        int diff = I - K;
-        MSE += diff*diff;
-    }
-    return MSE;
+  for( int i = 0; i < min_read_unit; i++ ) {
+    unsigned char I = c0[i];
+    unsigned char K = c1[i];
+    int diff = I - K;
+    MSE += diff*diff;
+  }
 }
 
 double MSE2PSNR
 ( double MSE
+, double MAX
 )
 {
-    const unsigned char MAX = 0xff;
-    double PSNR = INFINITY;
+  double PSNR = INFINITY;
 
-    if( DBL_EPSILON < MSE ) {
-        PSNR = 10 * log10( (MAX*MAX)/MSE );
-    }
+  if( DBL_EPSILON < MSE ) {
+    PSNR = 10 * log10( (MAX*MAX)/MSE );
+  }
 
-    return PSNR;
+  return PSNR;
 }
 
-class Option {
-
-public:
-    Option();
-    void parse(int argc, char* argv[]);
-
-public:
-    int nthread;
-    int width;
-    int height;
-    string file0;
-    string file1;
-    bool showFrame;
+struct Option {
+  int width;
+  int height;
+  string file0;
+  string file1;
+  bool showFrame;
 };
 
-Option::Option()
-: nthread(3)
-, width(352)
-, height(288)
-, file0("")
-, file1("")
-, showFrame(false)
+void parse_option
+( int argc
+, char* argv[]
+, Option& opt
+)
 {
-}
+  for( int i = 1; i < argc; i++ ) {
 
-void Option::parse(int argc, char* argv[])
-{
-    for( int i = 1; i < argc; i++ ) {
-
-        string op = argv[i];
+    string op = argv[i];
     
-        if( op == "-w" ) {
-            i++;
-            width = boost::lexical_cast<int>(argv[i]);
-        } else if( op == "-h" ) {
-            i++;
-            height = boost::lexical_cast<int>(argv[i]);
-        } else if( op == "-i0" ) {
-            i++;
-            file0 = argv[i];
-        } else if( op == "-i1" ) {
-            i++;
-            file1 = argv[i];
-        } else if( op == "-t" ) {
-            i++;
-            nthread = 3*boost::lexical_cast<int>(argv[i]);
-        } else if( op == "-v" ) {
-            showFrame = true;
-        }
+    if( op == "-w" ) {
+      i++;
+      opt.width = boost::lexical_cast<int>(argv[i]);
+    } else if( op == "-h" ) {
+      i++;
+      opt.height = boost::lexical_cast<int>(argv[i]);
+    } else if( op == "-i0" ) {
+      i++;
+      opt.file0 = argv[i];
+    } else if( op == "-i1" ) {
+      i++;
+      opt.file1 = argv[i];
+    } else if( op == "-v" ) {
+      opt.showFrame = true;
     }
-}
-
-class Fvalue 
-{
-public:
-
-    Fvalue(size_t n) {
-        c0vec.resize(n);
-        c1vec.resize(n);
-    }
-
-    Fvalue( Fvalue&& org ) {
-        f = move(org.f);
-        c0vec = org.c0vec;
-        c1vec = org.c1vec;
-    }
-
-    Fvalue& operator= ( Fvalue&& org ) {
-        f = move(org.f);
-        c0vec = org.c0vec;
-        c1vec = org.c1vec;
-        return *this;
-    }
-
-    future<double> f;
-    vector<unsigned char> c0vec;
-    vector<unsigned char> c1vec;
-};
-
-class Yuv2psnr {
-
-public:
-
-    Yuv2psnr(int argc, char* argv[]);
-    ~Yuv2psnr();
-    void check();
-    void run();
-
-private:
-
-    //
-    void sync_frame();
-
-    //
-    Option opt;
-    ifstream fin0;
-    ifstream fin1;
-    unsigned int area;
-    unsigned int read_unit;
-    int count;
-    int nframe;
-    double totalYMSE;
-    double totalUMSE;
-    double totalVMSE;
-    deque<Fvalue> fydeque;
-    deque<Fvalue> fudeque;
-    deque<Fvalue> fvdeque;
-};
-
-Yuv2psnr::Yuv2psnr(int argc, char* argv[])
-: count(0)
-, nframe(0)
-, totalYMSE(0.0)
-, totalUMSE(0.0)
-, totalVMSE(0.0)
-{
-    opt.parse(argc, argv);
-    area = opt.width*opt.height;
-    read_unit = area;
-    fin0.open( opt.file0.c_str(), ios::in | ios::binary );
-    fin1.open( opt.file1.c_str(), ios::in | ios::binary );
-}
-
-Yuv2psnr::~Yuv2psnr()
-{
-    fin0.close();
-    fin1.close();
-}
-
-void Yuv2psnr::check()
-{
-    if( fin0.fail() ){
-        stringstream ss;
-        ss << "Error: cannot open file `" << opt.file0 << "'";
-        throw ss.str();
-    }
-
-    if( fin1.fail() ){
-        stringstream ss;        
-        cout << "Error: cannot open file `" << opt.file1 << "'";
-        throw ss.str();
-    }
-}
-
-void Yuv2psnr::sync_frame()
-{
-    double frameYMSE = fydeque.front().f.get();
-    fydeque.pop_front();
-
-    double frameUMSE = fudeque.front().f.get();
-    fudeque.pop_front();
-
-    double frameVMSE = fvdeque.front().f.get();
-    fvdeque.pop_front();
-
-    totalYMSE += frameYMSE;
-    totalUMSE += frameUMSE;
-    totalVMSE += frameVMSE;
-
-    frameYMSE /= area;
-    frameUMSE /= area/4;
-    frameVMSE /= area/4;
-
-    if( opt.showFrame ) {
-
-        double y = MSE2PSNR(frameYMSE);
-        double u = MSE2PSNR(frameUMSE);
-        double v = MSE2PSNR(frameVMSE);
-        cout << boost::format("%-12d%-12.4f%-12.4f%-12.4f") % nframe % y % u % v << endl;
-    }
-
-    nframe++;
-}
-
-void Yuv2psnr::run()
-{
-    cout << "Frame       Y-PSNR[dB]  U-PSNR[dB]  V-PSNR[dB]" << endl;
-
-    while( !fin0.eof() ) {
-
-        Fvalue vf(read_unit);
-        auto c0 = (char*)&(vf.c0vec.at(0));
-        auto c1 = (char*)&(vf.c1vec.at(0));
-        fin0.read(c0, read_unit); 
-        fin1.read(c1, read_unit); 
-
-        count += read_unit;
-
-        auto f = async(launch::async, MSE, vf.c0vec, vf.c1vec);
-        vf.f = move(f);
-
-        // extract Y
-        if( count <= area ) { 
-            fydeque.push_back(move(vf));
-            read_unit = area/4;
-
-        // extract U
-        } else if( area < count && count <= area/4*5 ) {
-            fudeque.push_back(move(vf));
-
-        // extract V
-        } else if( area/4*5 < count ) {
-            fvdeque.push_back(move(vf));
-        }
-
-        // end of 1 frame
-        if( count == area/2*3 ) {
-            count = 0;
-            read_unit = area;
-        }
-
-        // sync
-        while( opt.nthread < fydeque.size() + fudeque.size() + fvdeque.size() ){
-            sync_frame();
-        }
-    }
-
-    // flush
-    while( !fvdeque.empty() ){
-        sync_frame();
-    }
-
-    totalYMSE /= area*nframe;
-    totalUMSE /= area/4*nframe;
-    totalVMSE /= area/4*nframe;
-
-    auto y = MSE2PSNR(totalYMSE);
-    auto u = MSE2PSNR(totalUMSE);
-    auto v = MSE2PSNR(totalVMSE);
-    cout << boost::format("Total       %-12.4f%-12.4f%-12.4f") % y % u % v << endl;
+  }
 }
 
 int main(int argc, char* argv[])
 {
-    try {
+  Option opt;
+  // CIF
+  opt.width = 352;
+  opt.height = 288;
+  // HD
+  //int width  = 1920;
+  //int height = 1080;
+  opt.file0 = "";
+  opt.file1 = "";
+  opt.showFrame = false;
+  parse_option(argc, argv, opt);
 
-        Yuv2psnr yuv2psnr(argc, argv);
-        yuv2psnr.run();
+  const unsigned int area = opt.width*opt.height;
+  //const unsigned int min_read_unit = 16;
+  //const unsigned int min_read_unit = 8;
+  unsigned int min_read_unit = area;
+  const unsigned char MAX = 255;
 
-    } catch(string& what) {
+  ifstream fin0( opt.file0.c_str(), ios::in | ios::binary );
+  ifstream fin1( opt.file1.c_str(), ios::in | ios::binary );
+    
+  if( !fin0 ){
+    cout << "Error: cannot open file `" << opt.file0 << "'" << endl;
+    return 1;
+  }
 
-        cerr << what << endl;
-        return 0x01;
+  if( !fin1 ){
+    cout << "Error: cannot open file `" << opt.file1 << "'" << endl;
+    return 1;
+  }
 
-    } catch(...) {
+  int count = 0;    
+  int nframe = 0;
+  unsigned char* c0 = new unsigned char[min_read_unit];
+  unsigned char* c1 = new unsigned char[min_read_unit];
 
-        cerr << "Error: unexpectd expection" << endl;
-        return 0x02;
+  double totalYMSE = 0.0;
+  double totalUMSE = 0.0;
+  double totalVMSE = 0.0;
+
+  double frameYMSE = 0.0;
+  double frameUMSE = 0.0;
+  double frameVMSE = 0.0;
+
+  cout << "Frame       Y-PSNR[dB]  U-PSNR[dB]  V-PSNR[dB]" << endl;
+
+  while( !fin0.eof() ){
+
+    fin0.read( (char*)c0, min_read_unit ); 
+    fin1.read( (char*)c1, min_read_unit ); 
+    count += min_read_unit;
+
+    // extract Y
+    if( count <= area ) { 
+      MSE(c0, c1, min_read_unit, frameYMSE);
+      min_read_unit = area/4;
+
+    // extract U
+    } else if( area < count && count <= area/4*5 ) { // YUV420
+      MSE(c0, c1, min_read_unit, frameUMSE);
+
+    // extract V
+    } else if( area/4*5 < count ) { // YUV420
+      MSE(c0, c1, min_read_unit, frameVMSE);
     }
 
-    return 0x00;
+    // end of 1 frame
+    if( count == area/2*3 ) {
+
+      totalYMSE += frameYMSE;
+      totalUMSE += frameUMSE;
+      totalVMSE += frameVMSE;
+
+      frameYMSE /= area;
+      frameUMSE /= area/4;
+      frameVMSE /= area/4;
+
+      if( opt.showFrame ) {
+
+        double y = MSE2PSNR(frameYMSE, MAX);
+        double u = MSE2PSNR(frameUMSE, MAX);
+        double v = MSE2PSNR(frameVMSE, MAX);
+        cout << boost::format("%-12d%-12.4f%-12.4f%-12.4f") % nframe % y % u % v << endl;
+      }
+
+      frameYMSE = 0.0;
+      frameUMSE = 0.0;
+      frameVMSE = 0.0;
+
+      nframe++;
+      count = 0;
+      min_read_unit = area;
+    }
+  }
+
+  totalYMSE /= area*nframe;
+  totalUMSE /= area/4*nframe;
+  totalVMSE /= area/4*nframe;
+
+  double y = MSE2PSNR(totalYMSE, MAX);
+  double u = MSE2PSNR(totalUMSE, MAX);
+  double v = MSE2PSNR(totalVMSE, MAX);
+  cout << boost::format("Total       %-12.4f%-12.4f%-12.4f") % y % u % v << endl;
+
+  delete [] c0;
+  delete [] c1;
+
+  fin0.close();
+  fin1.close();
+    
+  return 0;
 }
